9d7c983f86f49e6610704d11b3532e0a1166cd49
Avish Kaushik <avish.kaushik20@gmail.com>
2020-06-28 15:16:55 +0530

    Sorting Visualizer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx'
 
 function App() {
+  document.title = "Sorting Visualizer"
   return (
     <div className="App">
       <SortingVisualizer></SortingVisualizer>
